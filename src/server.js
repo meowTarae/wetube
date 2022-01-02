67f0aecc2c95f8c84a03692f9868dd690e2c17da
타래 <bae94157617@gmail.com>
2022-01-02 16:39:20 +0900

    ~#3.11

--- src/server.js
+++ src/server.js
@@ -1,7 +1,33 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const Port = 4000;
+
+const logger = morgan("dev");
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed !</h1>");
+  }
+  console.log("Allowed url");
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("Hi");
+};
+
+const handleProtected = (req, res) => {
+  return res.send("Protected lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Sever listening on port http://localhost:${Port}`);

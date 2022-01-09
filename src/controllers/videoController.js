97654c987e9d5446cc9f474728fab4d9275d34fa
타래 <bae94157617@gmail.com>
2022-01-09 20:03:21 +0900

    end #5

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,16 @@
-export const recomends = (req, res) => res.render("home");
+export const recomends = (req, res) => {
+  const videos = [
+    {
+      title: "Hello",
+    },
+    {
+      title: "Video #2",
+    },
+    {
+      title: "Whatsup",
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const watch = (req, res) => res.render("watch");
 export const edit = (req, res) => res.send("Edit Videos");

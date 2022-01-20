6e43b08bd7ec59323f83c940bd57db9806f8f782
타래 <bae94157617@gmail.com>
2022-01-20 17:47:57 +0900

    중간 점검
    자꾸 callback func 에러가 뜸

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,27 +1,55 @@
-import Video from "../models/Video";
-
+// import Video from "../models/Video";
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
 export const home = async (req, res) => {
-  const videos = await Video.find({});
-  return res.render("home", { pageTitle: "Home" });
+  // const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home" }, videos);
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
-
-  return res.render("watch", { pageTitle: `Watching: ${video.title}` });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
+  return res.render("edit", { pageTitle: `Editing` });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
+  return res.redirect(`/videos/${id}`);
 };
 
 export const getUpload = (req, res) => {
   const { id } = req.params;
+  return res.render("upload", { pageTitle: `Uploading` });
 };
 export const postUpload = (req, res) => {
   const { id } = req.params;
+  return res.redirect("/");
 };

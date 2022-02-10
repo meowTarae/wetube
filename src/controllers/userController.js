58c84c0bffb95dd02a5163b4b2e74f4740a8051f
타래 <85001878+meowTarae@users.noreply.github.com>
2022-02-10 21:58:02 +0900

    .

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,9 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = (req, res) => {
+  console.log(req.body);
+  res.end();
+};
 export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
-
 export const remove = (req, res) =>
   res.render("remove", { pageTitle: "Remove User" });
 export const logout = (req, res) =>

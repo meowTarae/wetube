7636743cccd78e06b8022e3baf8321ce696134ae
타래 <bae94157617@gmail.com>
2022-02-12 15:11:28 +0900

    .

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { remove, logout, see } from "../controllers/userController";
+import { remove, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/remove", remove);
-userRouter.get("/logout", logout);
 userRouter.get("/:id", see);
 
 export default userRouter;

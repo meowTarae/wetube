09922fb5fe6666a9b6c933efa0e75907b628db69
타래 <bae94157617@gmail.com>
2022-02-12 18:11:43 +0900

    ~#7.6

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,24 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+
   await User.create({
     name,
     username,
@@ -12,7 +28,30 @@ export const postJoin = async (req, res) => {
   });
   return res.redirect("/login");
 };
-export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+
+  return res.redirect("/");
+};
 export const remove = (req, res) =>
   res.render("remove", { pageTitle: "Remove User" });
 export const logout = (req, res) =>

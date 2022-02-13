418fed2dc30bb1d233f01abd7b819e431db5a369
타래 <bae94157617@gmail.com>
2022-02-13 19:30:29 +0900

    ~#7.13

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -41,7 +41,6 @@ export const postLogin = async (req, res) => {
       errorMessage: "An account with this username does not exists.",
     });
   }
-
   const ok = await bcrypt.compare(password, user.password);
   if (!ok) {
     return res.status(400).render("login", {
@@ -49,7 +48,8 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong password",
     });
   }
-
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 export const remove = (req, res) =>
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
@@ -13,6 +16,16 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    secret: "Hello!",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+  })
+);
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
